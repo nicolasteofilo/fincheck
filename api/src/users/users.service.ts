fb1eb4eb79440f005ba0de850b9cc3b38bc4037d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2024-03-23 20:30:39 -0300

    api(feat): add user in database

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('this email sent is in use');
+    }
+
+    const hashedPass = await hash(createUserDto.password, 8);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: hashedPass,
+      },
+    });
+
+    return user;
   }
 }
