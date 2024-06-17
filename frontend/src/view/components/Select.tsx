e290952693b4d9b29df222348fc43cce92f9e036
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2024-06-17 17:38:04 -0300

    front(feat): create Select component

---
+++ frontend/src/view/components/Select.tsx
@@ -0,0 +1,102 @@
+import * as RdxSelect from "@radix-ui/react-select";
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  CrossCircledIcon,
+} from "@radix-ui/react-icons";
+import { cn } from "../../utils/cn";
+import { useState } from "react";
+
+interface SelectProps {
+  className?: string;
+  error?: string;
+  placeholder?: string;
+  options: {
+    value: string;
+    label: string;
+  }[];
+}
+
+export function Select({
+  className,
+  error,
+  placeholder,
+  options,
+}: SelectProps) {
+  const [selectedValue, setSelectedValue] = useState<string>("");
+
+  function handleSelectValue(value: string) {
+    setSelectedValue(value);
+  }
+
+  return (
+    <div>
+      <div className="relative">
+        <label
+          htmlFor=""
+          className={cn(
+            "absolute z-10 top-1/2 -translate-y-1/2 left-3 text-gray-700 pointer-events-none",
+            selectedValue && "text-xs left-[13px] top-3.5 transition-all"
+          )}
+        >
+          {placeholder}
+        </label>
+
+        <RdxSelect.Root
+          onValueChange={(value) => handleSelectValue(value)}
+          value={selectedValue}
+        >
+          <RdxSelect.Trigger
+            className={cn(
+              "bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 focus:border-gray-800 transition-all outline-none flex justify-between items-center pt-4",
+              error && "border-red-900 focus:border-red-900",
+              className
+            )}
+          >
+            <RdxSelect.Value />
+            <RdxSelect.Icon className="h-full absolute right-3 top-4">
+              <ChevronDownIcon className="w-6 h-6 text-gray-800" />
+            </RdxSelect.Icon>
+          </RdxSelect.Trigger>
+
+          <RdxSelect.Portal>
+            <RdxSelect.Content
+              className={cn(
+                "overflow-hidden bg-white z-[99] rounded-2xl border border-gray-100"
+              )}
+            >
+              <RdxSelect.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-gray-800 cursor-default">
+                <ChevronUpIcon />
+              </RdxSelect.ScrollUpButton>
+
+              <RdxSelect.Viewport className="p-4">
+                {options.map((option) => (
+                  <RdxSelect.Item
+                    value={option.value}
+                    key={option.value}
+                    className="p-2 text-gray-800 text-sm cursor-pointer data-[state=checked]:font-bold outline-none data-[highlighted]:bg-gray-100 rounded-lg transition-colors"
+                  >
+                    <RdxSelect.ItemText>
+                      {option.label}
+                    </RdxSelect.ItemText>
+                  </RdxSelect.Item>
+                ))}
+              </RdxSelect.Viewport>
+
+              <RdxSelect.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-gray-800 cursor-default">
+                <ChevronDownIcon />
+              </RdxSelect.ScrollDownButton>
+            </RdxSelect.Content>
+          </RdxSelect.Portal>
+        </RdxSelect.Root>
+      </div>
+
+      {error && (
+        <div className="flex items-center gap-2 mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-xs">{error}</span>
+        </div>
+      )}
+    </div>
+  );
+}
--- frontend/src/view/pages/Dashboard/dialogs/NewAccountDialog/index.tsx
+++ frontend/src/view/pages/Dashboard/dialogs/NewAccountDialog/index.tsx
@@ -1,6 +1,7 @@
 import { Dialog } from "../../../../components/Dialog";
 import { Input } from "../../../../components/Input";
 import { InputCurreny } from "../../../../components/InputCurreny";
+import { Select } from "../../../../components/Select";
 import { useNewAccountDialogController } from "./useNewAccountDialogController";
 
 export function NewAccountDialog() {
@@ -22,8 +23,16 @@ export function NewAccountDialog() {
           </div>
         </div>
 
-        <div className="mt-10">
+        <div className="mt-10 flex flex-col gap-4">
           <Input type="text" name="name" placeholder="Nome da conta" />
+          <Select
+            placeholder="Tipo"
+            options={[
+              { value: "CHECKING", label: "Conta Corrente" },
+              { value: "INVESTMENT", label: "Investimentos" },
+              { value: "CASH", label: "Dinheiro Físico" },
+            ]}
+          />
         </div>
       </form>
     </Dialog>
